518b76cb2fa779ac260e4e3c37efb1c2341d4458
ts-3156 <ts_3156@yahoo.co.jp>
2025-04-01 10:54:04 +0900

    Fix config.assets.paths

--- app/assets/javascript/apple.js
+++ app/assets/javascript/apple.js
@@ -4,5 +4,6 @@ export class Apple {
 
   say() {
     console.log('Apple!');
+    console.log(RAILS_ASSET_URL("book.png"));
   }
 }
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,8 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+Rails.application.config.assets.excluded_paths = [
+    Rails.root.join("app/assets/javascript"),
+    Rails.root.join("app/assets/stylesheets")
+]
